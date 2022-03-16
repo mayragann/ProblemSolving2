f810321479ff70cc5e391248af7ad39b571baaae
Mayra <mayragann@gmail.com>
2022-03-16 17:07:25 -0400

    added main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+#In number theory, a happy number is a number which eventually reaches 1 when replaced by the sum of the square of each digit
+#On the other hand, 4 is not a happy number because the sequence starting with 4**2 he number that started the sequence,
+#and so the process continues in an infinite cycle without ever reaching 1. A number which is not happy is called sad or unhappy.
